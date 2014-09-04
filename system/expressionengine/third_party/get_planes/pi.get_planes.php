4346a3701a3b0513cfd214cd6662505522fd54f2
Freddy <freddy01288@gmail.com>
2014-09-04 18:40:27 -0500

    probando consulta plugin

--- system/expressionengine/third_party/get_planes/pi.get_planes.php
+++ system/expressionengine/third_party/get_planes/pi.get_planes.php
@@ -89,6 +89,54 @@ class Get_planes
         }
         return $form;
     }
+
+    public function get_planes_por_monto(){
+        $monto = ee()->TMPL->fetch_param('monto');
+
+        if (($monto > 0) && ($monto <= 55000)) {
+            $id_1 = 1;
+            $id_2 = 2;
+        }elseif (($monto > 55000) && ($monto <= 115000)) {
+            $id_1 = 3;
+            $id_2 = 4;            
+        }
+        }elseif (($monto > 115000) && ($monto <= 150000)) {
+            $id_1 = 5;
+            $id_2 = 6;
+        }
+        }elseif (($monto > 150000) && ($monto <= 300000)) {
+            $id_1 = 7;
+            $id_2 = 8;
+        }
+        }elseif (($monto > 300000) && ($monto <= 450000)) {
+            $id_1 = 9;
+            $id_2 = 10;
+        }
+        }elseif (($monto > 450000) && ($monto <= 600000)) {
+            $id_1 = 11;
+            $id_2 = 12;
+        }
+
+
+        $form = '';
+        ee()->db->where('id',$id_1);
+        ee()->db->or_where('id',$id_2);
+        $query = ee()->db->get('exp_planes_domiciliario');
+        foreach($query->result() as $row){
+            $robo = '';
+            if ($row->theft == 1) {
+                $robo = 'Sí';
+            }else{
+                $robo = 'No';
+            };
+            $form .= '<ul>';
+            $form .= '<li>Plan : '.$row->name.'</li>';
+            $form .= '<li>Con robo : '.$robo.'</li>';
+            $form .= '<li>Precio : '.$row->price.'</li>';
+            $form .= '</ul>';
+        }
+        return $form;
+    }    
   
 } 
 /* End of file pi.rating.php */
