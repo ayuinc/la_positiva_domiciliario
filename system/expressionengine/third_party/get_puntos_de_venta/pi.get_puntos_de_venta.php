a2692ac6feeb8dd2f98eba8923253882245d5ce0
Freddy <freddy01288@gmail.com>
2014-09-08 11:23:26 -0500

    agregando agencias de ventas

--- system/expressionengine/third_party/get_puntos_de_venta/pi.get_puntos_de_venta.php
+++ system/expressionengine/third_party/get_puntos_de_venta/pi.get_puntos_de_venta.php
@@ -71,7 +71,7 @@ class Get_puntos_de_venta
         ee()->db->where('distrito',$district);
         $query = ee()->db->get('exp_agencias');
         foreach($query->result() as $row){
-            $div .='<div class="result">';
+            $div .='<div>';
             $div .= '<p>'.$row->oficina.'</p>';
             $div .= '<p>'.$row->direccion.'</p>';
             $div .= '<p>'.$row->ubicacion.'</p>';
